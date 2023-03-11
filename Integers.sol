2b43357052bc220078cb296a10778c7f9019c10a
starrohan999 <starrohan999@gmail.com>
2023-03-11 22:07:43 +0530

    Integers concept--

---
+++ Integers.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier:UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract Integers
+{
+    //two types of integer declaration are there.
+    //1. int (signed integer)int256 {int8,int16,intt32}
+    // by default it intialize by 0.
+    int x=127;//{-128 to 127}
+    //2. uint(unsigned Integer)uint256
+    uint temp=0;
+}
